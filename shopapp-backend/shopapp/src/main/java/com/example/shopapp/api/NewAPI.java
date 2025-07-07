9fe477aa3ed19ce4111d00e051945f63c56ca81f
deadofhead123 <phamminhhoa3005@gmail.com>
2025-07-07 19:48:38 +0700

    chap-3-video-1

---
+++ shopapp-backend/shopapp/src/main/java/com/example/shopapp/api/NewAPI.java
@@ -0,0 +1,14 @@
+package com.example.shopapp.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NewAPI {
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to shopapp";
+    }
+}
