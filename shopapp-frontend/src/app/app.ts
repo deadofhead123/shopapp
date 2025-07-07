6e8ec66b06e82d560500df65109c27f81fa43ebf
deadofhead123 <phamminhhoa3005@gmail.com>
2025-07-07 09:14:34 +0700

    chap-1-video-5

--- shopapp-frontend/src/app/app.ts
+++ shopapp-frontend/src/app/app.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { OrderComponent } from './order/order';
+import { OrderConfirmComponent } from './order-confirm/order-confirm';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HomeComponent,
-    OrderComponent
+    OrderComponent,
+    OrderConfirmComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
