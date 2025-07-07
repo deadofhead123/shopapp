fb79d7785a2652f3699c88e3f0dcef6d9b870736
deadofhead123 <phamminhhoa3005@gmail.com>
2025-07-07 08:43:36 +0700

    chap-1-video-4

--- shopapp-frontend/src/app/home/home.ts
+++ shopapp-frontend/src/app/home/home.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CurrencyPipe } from '../shared/pipes/CurrencyPipe.pipe';
+import { HeaderComponent } from '../header/header';
+import { FooterComponent } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
   imports: [
-    CurrencyPipe
+    CurrencyPipe,
+    HeaderComponent,
+    FooterComponent,
+
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
