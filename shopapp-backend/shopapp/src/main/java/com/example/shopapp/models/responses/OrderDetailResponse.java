0c27f3c1b5db97659182d657d99622bd4f7a3fa1
deadofhead123 <phamminhhoa3005@gmail.com>
2025-07-11 16:17:01 +0700

    chap-5-video-8

--- shopapp-backend/shopapp/src/main/java/com/example/shopapp/models/responses/OrderDetailResponse.java
+++ shopapp-backend/shopapp/src/main/java/com/example/shopapp/models/responses/OrderDetailResponse.java
@@ -1,35 +1,23 @@
 package com.example.shopapp.models.responses;
 
 import com.example.shopapp.entities.OrderDetail;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OrderDetailResponse {
-    private Long id;
+    Long id;
+    Long orderId;
+    Long productId;
+    Float price;
+    int numberOfProducts;
+    Float totalMoney;
+    String color;
 
-    @JsonProperty("order_id")
-    private Long orderId;
-
-    @JsonProperty("product_id")
-    private Long productId;
-
-    @JsonProperty("price")
-    private Float price;
-
-    @JsonProperty("number_of_products")
-    private int numberOfProducts;
-
-    @JsonProperty("total_money")
-    private Float totalMoney;
-
-    private String color;
     public static OrderDetailResponse fromOrderDetail(OrderDetail orderDetail) {
         return OrderDetailResponse
                 .builder()
