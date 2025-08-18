6cca3ee0ec62329365cf682b140f51cdbef4259e
deadofhead123 <phamminhhoa3005@gmail.com>
2025-08-18 16:42:29 +0700

    chap-7-video-1

--- shopapp-frontend/src/app/app.ts
+++ shopapp-frontend/src/app/app.ts
@@ -6,6 +6,7 @@ import { OrderConfirmComponent } from './order-confirm/order-confirm';
 import { LoginComponent } from './login/login';
 import { RegisterComponent } from './register/register';
 import { DetailProductComponent } from './detail-product/detail-product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ import { DetailProductComponent } from './detail-product/detail-product';
     LoginComponent,
     RegisterComponent,
     DetailProductComponent,
+    FormsModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- shopapp-frontend/src/app/register/register.ts
+++ shopapp-frontend/src/app/register/register.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header';
 import { FooterComponent } from '../footer/footer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   imports: [
     HeaderComponent,
     FooterComponent,
+    FormsModule
   ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class RegisterComponent {
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  dateOfBirth: Date;
+  isAccepted: boolean;
 
+  constructor() {
+    this.phone = '';
+    this.password = '';
+    this.retypePassword = '';
+    this.fullName = '';
+    this.address = '';
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+    this.isAccepted = true;
+  }
+
+  onPhoneChange() {
+    console.log(this.phone);
+  }
+
+  register() {
+    alert(`Register successfully!
+            phone: ${this.phone},
+            password: ${this.password},
+            retypePassword: ${this.retypePassword},
+            full name: ${this.fullName},
+            address: ${this.address},
+            date of birth: ${this.dateOfBirth},
+            isAccepted: ${this.isAccepted}
+      `);
+  }
 }
